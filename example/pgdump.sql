c21d348966a9dad75b574d3490c0dcd828dea98e
Megan Ruggiero <decadentsoup@users.noreply.github.com>
2023-07-12 00:48:58 -0400

    Add test script

---
+++ example/pgdump.sql
@@ -0,0 +1,110 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 15.3
+-- Dumped by pg_dump version 15.3
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: account; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.account (
+    username text NOT NULL,
+    password text NOT NULL
+);
+
+
+ALTER TABLE public.account OWNER TO postgres;
+
+--
+-- Name: post; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.post (
+    title text NOT NULL,
+    body text NOT NULL
+);
+
+
+ALTER TABLE public.post OWNER TO postgres;
+
+--
+-- Name: sledger; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.sledger (
+    index bigint NOT NULL,
+    forward text NOT NULL,
+    backward text NOT NULL,
+    "timestamp" timestamp without time zone DEFAULT now() NOT NULL
+);
+
+
+ALTER TABLE public.sledger OWNER TO postgres;
+
+--
+-- Name: sledger_version; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.sledger_version (
+    sledger_version text
+);
+
+
+ALTER TABLE public.sledger_version OWNER TO postgres;
+
+--
+-- Data for Name: account; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.account (username, password) FROM stdin;
+\.
+
+
+--
+-- Data for Name: post; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.post (title, body) FROM stdin;
+\.
+
+
+--
+-- Data for Name: sledger; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.sledger (index, forward, backward, "timestamp") FROM stdin;
+0	CREATE TABLE account (username TEXT NOT NULL, password TEXT NOT NULL);	DROP TABLE account;	(timestamp)
+1	CREATE TABLE post (title TEXT NOT NULL, body TEXT NOT NULL);	DROP TABLE post;	(timestamp)
+\.
+
+
+--
+-- Data for Name: sledger_version; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.sledger_version (sledger_version) FROM stdin;
+a45a9821-8e0d-4126-8d99-0543e7f1f8f7
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
---
+++ scripts/test_docker.sh
@@ -0,0 +1,56 @@
+#!/bin/sh -e
+
+step() {
+	printf "\33[1m==> \33[35m$1\33[0m\n"
+}
+
+clean() {
+	docker rm --force sledger-test-pg
+	docker volume rm --force sledger-test-pg
+	docker network rm --force sledger-test
+}
+
+step "Change directory to the root."
+cd "$(dirname "$0")/.."
+
+step "Build the program to a special test tag."
+docker build --tag sledger:test .
+
+step "Clean up previous, failed runs if any."
+clean
+
+step "Set up a network for the containers to communicate."
+docker network create sledger-test
+
+step "Generate a password to use for the PostgreSQL database."
+password="$(pwgen --secure 64)"
+database="postgresql://postgres:$password@sledger-test-pg?sslmode=disable"
+
+step "Start up PostgreSQL for testing."
+docker run \
+	--rm \
+	--name sledger-test-pg \
+	--network sledger-test \
+	--detach \
+	--env POSTGRES_PASSWORD="$password" \
+	--mount type=volume,source=sledger-test-pg,destination=/var/lib/postgresql/data \
+	postgres:15-alpine
+
+step "Wait until PostgreSQL is ready."
+until docker exec sledger-test-pg pg_isready; do true; done
+
+step "Run sledger using our example ledger."
+docker run \
+	--rm \
+	--network sledger-test \
+	--mount type=bind,source="$PWD/example",destination=/ledger,readonly \
+	sledger:test \
+	--database "$database"
+
+step "Verify the database dump matches expectations."
+docker exec sledger-test-pg pg_dump "$database" |
+	sed 's/[0-9][0-9]*-[0-9][0-9]-[0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9]\.[0-9][0-9]*/(timestamp)/g' |
+	diff example/pgdump.sql -
+
+step "Clean up after ourselves."
+clean
